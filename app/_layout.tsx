f9885341a345e9bdb8a5d38f035e078f64b600ba
Cassie Lin <ineslin35@gmail.com>
2023-05-07 17:04:55 +0800

    feat: the welcome page's UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,26 @@
-import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+    const [fontLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    });
+
+    const handleLayoutRootView = useCallback(async () => {
+        if (fontLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontLoaded]);
+
+    if (!fontLoaded) return null;
+
     return (
-        <Stack />
+        <Stack onLayout={handleLayoutRootView} />
     )
 }
 
